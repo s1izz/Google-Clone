c000fa73a831fa0ab76ab2466fc656683d6c2ed6
slizz <cadenneary@gmail.com>
2022-09-30 08:25:27 -0500

    Push1

---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@mui/icons-material/Apps";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Search from "../Search";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__header--left">
+          <Link to="about">About</Link>
+          <Link to="store">Store</Link>
+        </div>
+        <div className="home__header--right">
+          <Link to="gmail">Gmail</Link>
+          <Link to="images">Images</Link>
+          <AppsIcon />
+          <AccountCircleIcon />
+        </div>
+      </div>
+
+      <div className="home__body">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"></img>
+        <div className="home__input--container">
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
